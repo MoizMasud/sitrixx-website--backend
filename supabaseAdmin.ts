f4dd41392a842a79bcc2584d044c9da1d0eca296
Moiz Masud <moizkhan_007@hotmail.com>
2025-12-12 13:27:50 -0500

    Update supabaseAdmin.ts

--- supabaseAdmin.ts
+++ supabaseAdmin.ts
@@ -4,5 +4,14 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// This client is ONLY for server-side use (never send this key to the browser)
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+// Server-only admin client
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  supabaseServiceKey,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
+);
